b3212a0749c768b383d05dcb72ae56bf0f06eea8
Edward Helman <eddhelman@gmail.com>
2016-02-20 16:18:53 -0500

    refactored code

--- change.rb
+++ change.rb
@@ -1,17 +1,16 @@
 def make_change(amount)
 	coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
 	
-		if amount == 5
-			coins = {"quarters" => 0, "dimes" => 0, "nickles" => 1, "pennies" => 0}
+		
+		if amount >= 5
+			coins = {"quarters" => 0, "dimes" => 0, "nickles" => 1, "pennies" => amount - 5}
 		end
 		
 		if amount < 5
 			coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => amount}
 		end
 		
-		if amount > 5
-			coins = {"quarters" => 0, "dimes" => 0, "nickles" => 1, "pennies" => amount - 5}
-		end
+		
 	coins
 
 
