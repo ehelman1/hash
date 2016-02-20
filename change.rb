e55a50f94aa7de9f038123a7d7f4c091cf26a57c
Edward Helman <eddhelman@gmail.com>
2016-02-20 17:00:23 -0500

    refactored 2

--- change.rb
+++ change.rb
@@ -1,30 +1,23 @@
 def make_change(amount)
 	coins = {"quarters" => 0, "dimes" => 0, "nickles" => 0, "pennies" => 0}
+	dimes = 10
+	nickles = 5
+	pennies = 1
 	
-		if amount >= 15
-			coins["dimes"] = 1
-			coins["nickles"] = 1
-			coins["pennies"] = amount - 15
-		
-		elsif amount >= 10
-			coins["dimes"] = 1
-			coins["pennies"] = amount - 10
-		
-		
-		elsif amount >= 5 
-			coins["nickles"] = 1
-			coins["pennies"] = amount - 5
-		
-		else amount < 5
-			coins["pennies"] = amount
-		end
-		
-		
+	if amount >= dimes
+		coins["dimes"] = 1
+		amount = amount - dimes
+	end
+	
+	if amount >= nickles
+		coins["nickles"] = 1
+		amount = amount - nickles
+	end
+	
+	if amount >= pennies
+		coins["pennies"] = amount
+	end
+				
 	coins
-
-
-
-
-
 
 end
